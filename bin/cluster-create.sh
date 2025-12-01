7e1ca66ec4c125e3c7cc3d8b798ff95749eba8d7
Julio C Bertelli <33905485+jcbrtl@users.noreply.github.com>
2025-12-01 19:58:38 -0300

    cluster create|delete: a way for dev/lab setup scripting

---
+++ bin/cluster-create.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+#
+# cluster-create.sh launches a new K3s cluster on virtual 'nodes'.
+# Please review and define your cluster-create.config .
+#
+# For simplicity, the first node created will be the single K8s master.
+#
+# KVM, libvirt and cloud-init utilities are required:
+#   sudo apt update && sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst cloud-image-utils
+# autok3s (https://github.com/cnrancher/autok3s/releases) is required.
+
+
+# configuration: start
+#
+# wrk is where we contain the setup
+wrk=~/kvm-lab
+#
+# baseimage is the KVM, bootable OS cloud image
+# https://cloud-images.ubuntu.com/minimal/releases/jammy/release/
+baseimage="${wrk}/baseimages/ubuntu-22.04-minimal-cloudimg-amd64.img"
+#
+# os_variant hints specific operating system configuration
+# must be one of:
+# $ virt-install --os-variant list
+os_variant="ubuntu22.04"
+#
+# ssh_user is the default user with sudo privileges
+ssh_user="ubuntu"
+#
+# user_ssh_key defines the pair to be used for installing k3s
+# (and ssh in general); generate with ssh-keygen, no password
+user_ssh_key=~/.ssh/kvm_lab_rsa
+#
+# nodes configuration
+node_memory=2048
+node_vcpus=2
+node_network="default"
+#
+# configuration: end
+
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 cluster-name cluster-size"
+	echo "Example: $0 my-lab 2"
+	exit
+fi
+
+for d in "qemu-img virt-install virsh nc autok3s"; do
+	if ! command -v $d &> /dev/null; then
+  	  echo "'${d}' is required but not found" >&2
+  	  exit 1
+	fi
+done
+
+cluster="$1"
+size="$2"
+
+if [ "$size" -lt 2 ]; then
+	size=2
+fi
+
+if [ -d "${wrk}/$cluster" ]; then
+	echo "$cluster exists - won't operate. can 'cluster-delete.sh ${cluster}'"
+	exit 1
+fi
+
+mkdir -pv "${wrk}/$cluster"
+	
+for n in $(seq 1 "$size"); do
+	node="${cluster}-node-${n}"
+
+	init="${wrk}/${cluster}/${node}-cloud-init"
+
+	cat > $init <<EOF
+#cloud-config
+hostname: ${node}
+fqdn: ${node}.local
+manage_etc_hosts: true
+users:
+  - name: ${ssh_user}
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    groups: users, admin
+    shell: /bin/bash
+    ssh_authorized_keys:
+      - $(cat ${user_ssh_key}.pub)
+ssh_pwauth: false
+disable_root: false
+#package_update: true
+packages:
+  - qemu-guest-agent # Important for libvirt to see IP addresses
+runcmd:
+  - systemctl restart ssh
+  - echo "Node is ready!"
+EOF
+
+	disk="${wrk}/${cluster}/${node}-disk.qcow2"
+	cp "$baseimage" "$disk"
+	qemu-img resize "$disk" +30G
+
+	sudo virt-install   \
+		--name "$node"   \
+		--memory "$node_memory"   \
+		--vcpus "$node_vcpus"   \
+		--disk path="${disk}",device=disk,bus=virtio \
+	      	--os-variant "$os_variant"    \
+		--network "network=${node_network},model=virtio"   \
+		--graphics none   \
+		--import   \
+		--noautoconsole   \
+		--cloud-init user-data="${init}"
+
+	echo "waiting for $node ..."
+
+	while [ -z "$node_ip" ]; do
+		node_ip=$(sudo virsh domifaddr "$node" | grep ipv4 | awk '{print $4}' | cut -f1 -d'/')
+		sleep 1
+	done
+
+	while ! nc -z $node_ip 22; do
+		sleep 1
+	done
+
+	node_ips+=$node_ip
+	if [ "$n" -ne "$size" ]; then
+		node_ips+=","
+	fi
+
+	echo $node_ip > "${wrk}/${cluster}/${node}-ip"
+
+	unset node_ip
+done
+
+cat <<< $node_ips
+
+master=$(echo $node_ips | cut -f1 -d',')
+workers=$(echo $node_ips | cut -f2- -d',')
+
+autok3s -d create     \
+	--provider native     \
+	--name "$cluster"     \
+	--ssh-user "$ssh_user"     \
+	--ssh-key-path "$user_ssh_key"     \
+	--master-ips "$master" \
+	--worker-ips "$workers"
+
+KUBECONFIG="$HOME/.autok3s/.kube/config" kubectl config view --minify --flatten > "${wrk}/${cluster}.kubeconfig"
+echo "done. run KUBECONFIG="${wrk}/${cluster}.kubeconfig" kubectl get nodes"
---
+++ bin/cluster-delete.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#
+# cluster-delete.sh cleans up a setup made by cluster-create.sh .
+#
+# KVM, libvirt and cloud-init utilities are required:
+#   sudo apt update && sudo apt install -y qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst cloud-image-utils
+# autok3s (https://github.com/cnrancher/autok3s/releases) is required.
+
+# configuration: start
+#
+# wrk is where we contain the setup
+wrk=~/kvm-lab
+#
+# configuration: end
+
+if [ $# -ne 1 ]; then
+        echo "Usage: $0 cluster-name"
+        echo "Example: $0 my-lab"
+        exit
+fi
+
+for d in "virsh autok3s"; do
+        if ! command -v $d &> /dev/null; then
+          echo "'${d}' is required but not found" >&2
+          exit 1
+        fi
+done
+
+cluster="$1"
+
+if [ ! -d "${wrk}/$cluster" ]; then
+        echo "$cluster doesn't exist - won't operate. clusters:"
+	autok3s list
+        exit 1
+fi
+
+nodes=$(sudo virsh list | grep "${cluster}-node-" | awk '{print $2}')
+
+if [ -z "$nodes" ]; then
+	echo "could not find a node. nodes:"
+	sudo virsh list
+	exit 1
+fi
+
+autok3s -d delete --provider native --name "$cluster"
+
+for n in $nodes; do
+	sudo virsh undefine $n
+	sudo virsh destroy $n
+done
+
+rm -rvi "${wrk}/${cluster}" "${wrk}/${cluster}.kubeconfig"
+
